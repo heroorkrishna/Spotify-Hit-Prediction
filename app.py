e5fa421efbf2e30f5309986b95cb9702924c9523
Krishna Heroor <heroorkrishna@gmail.com>
2020-10-30 11:52:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import flask
+from flask import Flask, render_template, request
+from spotify_api.fetch_data import *
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thecodex'
+
+
+@app.route('/', methods=["GET"])
+def home():
+   return render_template('index.html')
+
+@app.route('/', methods=["GET", "POST"])
+def predict():
+   title = request.form['song_title']
+   artist = request.form['artist_name']
+   output = make_prediction(title, artist)
+   
+   return render_template('prediction.html', prediction=output)
+
+
+
+if __name__ == '__main__':
+       app.run()
+
